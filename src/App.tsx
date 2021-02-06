8305b7ca7a7b814ca0bbf3095edaa5348bdfebd5
Hidekazu <tanakahidek@gmail.com>
2021-02-06 19:52:39 +0900

    開発環境の作成

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./styles/app.module.scss";
+
+export const App = () => {
+  return <div className={styles.appTxt}>App!!!</div>;
+};
---
+++ webpack.common.js
@@ -0,0 +1,105 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
+
+module.exports = {
+  entry: {
+    app: "./src/index.tsx",
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new ForkTsCheckerWebpackPlugin(),
+    new Dotenv(),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(j|t)sx?$/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              cacheDirectory: true,
+              presets: [
+                [
+                  "@babel/preset-env",
+                  {
+                    useBuiltIns: "usage",
+                    corejs: "3",
+                  },
+                ],
+                "@babel/preset-typescript",
+                "@babel/preset-react",
+              ],
+              plugins: ["@babel/plugin-transform-async-to-generator"],
+            },
+          },
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.scss$/,
+        exclude: /\.module\.scss$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+              modules: {
+                compileType: "icss",
+              },
+            },
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+      {
+        test: /\.module\.scss$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+              modules: {
+                compileType: "module",
+              },
+            },
+          },
+          {
+            loader: "sass-loader",
+          },
+          {
+            loader: "sass-resources-loader",
+            options: {
+              resources: "src/styles/shared/*.scss",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+  },
+};
